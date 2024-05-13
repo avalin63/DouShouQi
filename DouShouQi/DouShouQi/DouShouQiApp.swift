46ef85b565f7907cac493872129bf12dbfd91b32
ludelanier <lucas.delanier@etu.uca.fr>
2024-05-13 08:52:28 +0200

    init iOS App

---
+++ DouShouQi/DouShouQi/DouShouQiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouShouQiApp.swift
+//  DouShouQi
+//
+//  Created by Lucas Delanier on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DouShouQiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
