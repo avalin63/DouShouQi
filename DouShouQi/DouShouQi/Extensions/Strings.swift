e98af562797852301121960c9dc75527df60796e
ludelanier <lucas.delanier@etu.uca.fr>
2024-05-31 08:31:40 +0200

    add i18n on options

---
+++ DouShouQi/DouShouQi/Extensions/Strings.swift
@@ -0,0 +1,18 @@
+//
+//  Strings.swift
+//  DouShouQi
+//
+//  Created by Lucas Delanier on 31/05/2024.
+//
+
+import SwiftUI
+
+struct Strings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Strings()
+}
